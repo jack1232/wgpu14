13e4fb574913f4e571eb52ae7f8caa05d900736f
jack1232 <jackjack5810@yahoo.com>
2023-09-04 11:55:08 -0400

    update to 0.17

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 mod common;
-mod vertex_data;
-
-fn create_vertices(r_torus: f32, r_tube: f32, n_torus: usize, n_tube:usize) -> Vec<common::Vertex> {
-    let(pos, normal) = vertex_data::torus_data(r_torus, r_tube, n_torus, n_tube);
-    let mut data:Vec<common::Vertex> = Vec::with_capacity(pos.len());
-    for i in 0..pos.len() {
-        data.push(common::vertex(pos[i], normal[i]));
-    }
-    data.to_vec()
-}
+mod math_func;
 
 fn main(){
-    let vertex_data = create_vertices(1.5, 0.4, 100, 50);
-    let light_data = common::light([1.0,0.0,0.0], [1.0, 1.0, 0.0], 0.1, 0.6, 0.3, 30.0);
-    common::run(&vertex_data, light_data, "torus");
+    let mut colormap_name = "jet";
+    let mut is_two_side:i32 = 1;
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        colormap_name = &args[1];
+    }
+    if args.len() > 2 {
+        is_two_side = args[2].parse().unwrap();
+    }    
+
+    let vertex_data = common::create_vertices(&math_func::sinc, colormap_name, -8.0, 8.0, -8.0, 8.0, 
+        30, 30, 2.0, 0.7);
+    let light_data = common::light([1.0, 1.0, 1.0], 0.1, 0.8, 0.4, 30.0, is_two_side);
+    common::run(&vertex_data, light_data, colormap_name, "sinc");
 }
